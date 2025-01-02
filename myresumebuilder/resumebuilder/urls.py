4ed1897f149ef0c9fcd10f559ee6a3e6ee946cc2
Kuan Jia Hong <kuanjiahong@gmail.com>
2025-01-02 22:37:04 +0800

    Add basic CRUD route

--- myresumebuilder/resumebuilder/urls.py
+++ myresumebuilder/resumebuilder/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:resume_id>/", views.read, name="read"),
+    path("create", views.create, name="create"),
+    path("delete", views.delete, name="delete"),
+    path("update", views.update, name="update"),
 ]
