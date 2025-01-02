32349a660fb697c907a80032286a7b83c76d7c73
Kuan Jia Hong <jia-hong.kuan@polyu.edu.hk>
2025-01-02 17:13:16 +0800

    Add Resume model

--- myresumebuilder/resumebuilder/models.py
+++ myresumebuilder/resumebuilder/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Resume(models.Model):
+    # Required
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    education = models.TextField()
+    work_experience = models.TextField()
+    country_code = models.ForeignKey(CountryCode, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+
+    # Optional
+    summary = models.TextField(blank=True, null=True)
+    skills = models.TextField(blank=True, null=True)
+    certifications = models.TextField(blank=True, null=True)
+    projects = models.TextField(blank=True, null=True)
+    languages = models.TextField(blank=True, null=True)
+    hobbies = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}'s Resume"
+
+
