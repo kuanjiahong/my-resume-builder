d0666867d3e0fc5ace01080db5d06f90efb2b4a3
Kuan Jia Hong <jia-hong.kuan@polyu.edu.hk>
2025-01-03 14:58:00 +0800

    Add script to set env var in Unix platform

---
+++ scripts/unix-set-env-dev.sh
@@ -0,0 +1,15 @@
+export DEBUG=True
+export SECRET_KEY=
+export SESSION_COOKIE_SECURE=False
+export CSRF_COOKIE_SECURE=False
+export ALLOWED_HOSTS="localhost, 127.0.0.1"
+export SECURE_SSL_REDIRECT=False
+export SECURE_HSTS_SECONDS=0 # one hour
+export SECURE_HSTS_INCLUDE_SUBDOMAINS=False
+export SECURE_HSTS_PRELOAD=False
+
+# Database
+export DB_HOST=localhost
+export POSTGRES_DB=myresumebuilder
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=
