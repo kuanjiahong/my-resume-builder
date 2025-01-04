bf5e84e4dd2866367df5ea9475cc6b533f01d8a4
Kuan Jia Hong <kuanjiahong@gmail.com>
2025-01-04 12:13:10 +0800

    Add landing page at root path

--- myresumebuilder/myresumebuildersite/urls.py
+++ myresumebuilder/myresumebuildersite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from resumebuilder import views
 
 urlpatterns = [
+    path("", views.landing_page, name="landing_page"),
     path("resumebuilder/", include("resumebuilder.urls")),
     path('admin/', admin.site.urls),
 ]
